39b8d31e3abdc4ff290999ca05907e9257505f01
naptestdev <naptestfordev@gmail.com>
2024-04-09 20:48:17 +0700

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+import { auth } from "./firebase.js";
+import {
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
+
+window.handleSignOut = () => signOut(auth);
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    document.querySelector("#action-section").innerHTML = /*html*/ `
+    <a href="./login.html" class="btn btn-primary">Đăng nhập</a>
+  `;
+  } else {
+    document.querySelector("#action-section").innerHTML = /*html*/ `
+    <img style="width: 30px; height: 30px; border-radius: 999px; display: inline" src="https://api.dicebear.com/8.x/initials/svg?seed=${encodeURIComponent(
+      user.displayName
+    )}" />
+    <button style="display: inline-block" class="btn btn-primary" onclick="window.handleSignOut()">Đăng xuất</button>
+  `;
+  }
+});
+
+window.bookTour = async () => {
+  if (!auth.currentUser) {
+    Swal.fire({
+      title: "Hãy đăng nhập để đặt tour",
+      icon: "error",
+    });
+    return;
+  }
+
+  const { value: formValues } = await Swal.fire({
+    title: "Đặt Tour",
+    html: `
+    <p>(Để lại SĐT và email, chúng tôi sẽ liên lạc với bạn)</p>
+    <input type="tel" id="swal-input1" class="swal2-input" required placeholder="Số điện thoại">
+    <input type="email" id="swal-input2" class="swal2-input" required placeholder="Email">
+  `,
+    focusConfirm: false,
+    preConfirm: () => {
+      return [
+        document.getElementById("swal-input1").value,
+        document.getElementById("swal-input2").value,
+      ];
+    },
+  });
+  if (formValues.every((item) => item.trim())) {
+    Swal.fire(
+      "Đặt Tour thành công. Chúng tôi sẽ liên lạc với bạn sớm nhất có thể"
+    );
+  }
+};
